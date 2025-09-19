5b1fa3e62015a75298cd7fb9f9521112aceab5e9
KhalilKahwaji <khellowz@gmail.com>
2025-09-19 14:21:13 +0300

    links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,16 +10,16 @@ export default function Footer() {
       <div style={{ maxWidth: 1100, margin: "0 auto", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
         <small>© {new Date().getFullYear()} Thinkers C.C.C</small>
         <div style={{ display: "flex", gap: 12 }}>
-          <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <a href="https://www.instagram.com/thinkersccc/" target="_blank" rel="noreferrer">
             <img src={ig} alt="Instagram" width={24} height={24} />
           </a>
-          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+          <a href="https://www.facebook.com/ThinkersZahle" target="_blank" rel="noreferrer">
             <img src={fb} alt="Facebook" width={24} height={24} />
           </a>
-          <a href="https://tiktok.com" target="_blank" rel="noreferrer">
+          <a href="https://www.tiktok.com/@thinkers.ccc" target="_blank" rel="noreferrer">
             <img src={tt} alt="TikTok" width={24} height={24} />
           </a>
-         <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+         <a href="https://www.linkedin.com/company/thinkersccc/" target="_blank" rel="noreferrer">
             <img src={li} alt="Linkedin" width={24} height={24} />
           </a>
         </div>
